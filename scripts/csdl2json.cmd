3f9b087792a4fb97d855886f877f9f7c09cebc57
Gunnar Billung-Meyer <bil@christmann.info>
2017-04-20 12:40:03 +0200

    add conversion scripts

---
+++ scripts/csdl2json.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+SET PYTHON_EXE=C:\Users\bil.CIM\AppData\Local\Programs\Python\Python36-32\python.exe
+
+SET REDFISH_TOOLS_DIR=C:\Users\bil.CIM\workspace\other\Redfish-Tools
+SET CONVERTOR_APP_DIR=%REDFISH_TOOLS_DIR%\csdl-to-json-convertor
+SET CONVERTOR_SCRIPT=%CONVERTOR_APP_DIR%\service.py
+
+SET PROJECT_DIR=C:\Users\bil.CIM\workspace\other\recs-redfish-api
+SET CSDL_DIR=%PROJECT_DIR%\csdlSchemas
+SET JSON_DIR=%PROJECT_DIR%\schemas
+SET LOG_DIR=%PROJECT_DIR%\logs
+SET LOG_FILE=%LOG_DIR%\csdl2json.log
+
+"%PYTHON_EXE%" "%CONVERTOR_SCRIPT%" --directory "%CSDL_DIR%" --outdir "%JSON_DIR%" > "%LOG_FILE%" 2>&1
+
+rem delete empty schema
+del "%JSON_DIR%\Validation.v1_0_0.json"
+
+"%LOG_FILE%"
---
+++ scripts/json2html.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+SET PYTHON_EXE=C:\Users\bil.CIM\AppData\Local\Programs\Python\Python36-32\python.exe
+
+SET REDFISH_TOOLS_DIR=C:\Users\bil.CIM\workspace\other\Redfish-Tools
+SET DOC_APP_DIR=%REDFISH_TOOLS_DIR%\doc-generator
+SET DOC_SCRIPT=%DOC_APP_DIR%\doc_generator.py
+SET DOC_SUPP_FILE=%DOC_APP_DIR%\customsupplement.md
+
+SET PROJECT_DIR=C:\Users\bil.CIM\workspace\other\recs-redfish-api
+SET JSON_DIR=%PROJECT_DIR%\schemas
+SET DOC_DIR=%PROJECT_DIR%\documentation
+SET DOC_FILE=%DOC_DIR%\recsbox-redfish-api.html
+SET LOG_DIR=%PROJECT_DIR%\logs
+SET LOG_FILE=%LOG_DIR%\json2html.log
+
+"%PYTHON_EXE%" "%DOC_SCRIPT%" --format html --out "%DOC_FILE%" --sup "%DOC_SUPP_FILE%" "%JSON_DIR%" > "%LOG_FILE%" 2>&1
+
+"%DOC_FILE%"
+"%LOG_FILE%"
