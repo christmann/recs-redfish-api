24ed1480cafe6c44469fcaca5ae6b2e68b3f7e9f
Gunnar Billung-Meyer <bil@christmann.info>
2017-05-15 11:41:41 +0200

    read all CSDL files (not only OEM, because of the than missing dependencies)

--- scripts/csdl2json.cmd
+++ scripts/csdl2json.cmd
@@ -7,20 +7,19 @@ SET CONVERTOR_APP_DIR=%REDFISH_TOOLS_DIR%\csdl-to-json-convertor
 SET CONVERTOR_SCRIPT=%CONVERTOR_APP_DIR%\service.py
 
 SET PROJECT_DIR=C:\Users\bil.CIM\workspace\other\recs-redfish-api
-SET CSDL_DIR=%PROJECT_DIR%\csdlSchemas\OEM
+SET CSDL_DIR=%PROJECT_DIR%\csdlSchemas
 SET JSON_DIR=%PROJECT_DIR%\schemas\OEM
 SET LOG_DIR=%PROJECT_DIR%\logs
 SET LOG_FILE=%LOG_DIR%\csdl2json.log
 
 "%PYTHON_EXE%" "%CONVERTOR_SCRIPT%" --directory "%CSDL_DIR%" --outdir "%JSON_DIR%" > "%LOG_FILE%" 2>&1
 
-rem delete empty schema
-del "%JSON_DIR%\Validation.v1_0_0.json" >> "%LOG_FILE%" 2>&1
-
 if errorlevel 1 (
   echo Failed
   "%LOG_FILE%"
   exit /b %errorlevel%
 ) else (
+  rem delete empty schema
+  del "%JSON_DIR%\Validation.v1_0_0.json" >> "%LOG_FILE%" 2>&1
   echo Success
 )
