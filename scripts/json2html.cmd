f7763024b15d256ee3d85d39f1f819ea75439ab9
Gunnar Billung-Meyer <bil@christmann.info>
2017-07-03 14:01:28 +0200

    add possibility to filter Properties

--- scripts/json2html.cmd
+++ scripts/json2html.cmd
@@ -8,9 +8,10 @@ SET CSDL_DIR=%PROJECT_DIR%\csdlSchemas
 SET JSON_DIR=%PROJECT_DIR%\schemas
 SET DOC_DIR=%PROJECT_DIR%\documentation
 SET CONF_DIR=%PROJECT_DIR%\config
+SET FILTER_FILE=%CONF_DIR%\filter.json
 SET OEM_PREFIXES=EID_47597_RECSBox
 
-"%JAVA_EXE%" -jar "%APPLICATION%" --json "%JSON_DIR%" --csdl "%CSDL_DIR%" --doc "%DOC_DIR%" --oem "%OEM_PREFIXES%" 2>&1
+"%JAVA_EXE%" -jar "%APPLICATION%" --json "%JSON_DIR%" --csdl "%CSDL_DIR%" --doc "%DOC_DIR%" --oem "%OEM_PREFIXES%" --filter "%FILTER_FILE%" 2>&1
 
 if errorlevel 1 (
   echo Generation of the documentation failed!
