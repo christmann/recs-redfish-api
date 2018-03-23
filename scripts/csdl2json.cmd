1e0e3f4783da33858b0555988d978e01cb84e6d8
Gunnar Billung-Meyer <bil@christmann.info>
2018-03-23 13:10:05 +0100

    change schema to 1.4.0

--- scripts/csdl2json.cmd
+++ scripts/csdl2json.cmd
@@ -3,13 +3,13 @@
 SET PYTHON_EXE=%LOCALAPPDATA%\Programs\Python\Python36-32\python.exe
 
 SET PROJECT_DIR=..
-SET APPLICATION=%PROJECT_DIR%\applications\csdl-to-json-convertor\service.py
+SET APPLICATION=%PROJECT_DIR%\applications\csdl-to-json-convertor\csdl-to-json.py
 SET CSDL_DIR=%PROJECT_DIR%\csdlSchemas
 SET JSON_DIR=%PROJECT_DIR%\schemas\OEM
 SET LOG_DIR=%PROJECT_DIR%\logs
 SET LOG_FILE=%LOG_DIR%\csdl2json.log
 
-"%PYTHON_EXE%" "%APPLICATION%" --directory "%CSDL_DIR%" --outdir "%JSON_DIR%" > "%LOG_FILE%" 2>&1
+"%PYTHON_EXE%" "%APPLICATION%" --input "%CSDL_DIR%" --output "%JSON_DIR%"> "%LOG_FILE%" 2>&1
 
 if errorlevel 1 (
   echo Failed
